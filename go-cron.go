97e19bca08a49b656f768ff96b4083a6ecb327ee
codeskyblue <codeskyblue@gmail.com>
2015-08-17 16:14:31 +0800

    fix for dayli use

--- go-cron.go
+++ go-cron.go
@@ -1,69 +1,68 @@
 package main
-import "os"
-import "os/exec"
-import "strings"
-import "sync"
-import "os/signal"
-import "syscall"
-import "github.com/robfig/cron"
 
-func execute(command string, args []string)() {
+import (
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 
-    println("executing:", command, strings.Join(args, " "))
+	"github.com/robfig/cron"
+)
 
-    cmd := exec.Command(command, args...)
+func execute(command string, args []string) {
+	log.Printf("executing: %s %s", command, strings.Join(args, " "))
 
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-
-    cmd.Run()
-
-    cmd.Wait()
+	cmd := exec.Command(command, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
 
 func create() (cr *cron.Cron, wgr *sync.WaitGroup) {
-    var schedule string = os.Args[1]
-    var command string = os.Args[2]
-    var args []string = os.Args[3:len(os.Args)]
+	var schedule string = os.Args[1]
+	var command string = os.Args[2]
+	var args []string = os.Args[3:len(os.Args)]
 
-    wg := &sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
 
-    c := cron.New()
-    println("new cron:", schedule)
+	c := cron.New()
+	println("new cron:", schedule)
 
-    c.AddFunc(schedule, func() {
-        wg.Add(1)
-        execute(command, args)
-        wg.Done()
-    })
+	c.AddFunc(schedule, func() {
+		wg.Add(1)
+		execute(command, args)
+		wg.Done()
+	})
 
-    return c, wg
+	return c, wg
 }
 
 func start(c *cron.Cron, wg *sync.WaitGroup) {
-    c.Start()
+	c.Start()
 }
 
 func stop(c *cron.Cron, wg *sync.WaitGroup) {
-    println("Stopping")
-    c.Stop()
-    println("Waiting")
-    wg.Wait()
-    println("Exiting")
-    os.Exit(0)
+	println("Stopping")
+	c.Stop()
+	println("Waiting")
+	wg.Wait()
+	println("Exiting")
+	os.Exit(0)
 }
 
 func main() {
+	flag.Parse()
 
-    c, wg := create()
+	c, wg := create()
+	go start(c, wg)
 
-    go start(c, wg)
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
 
-    ch := make(chan os.Signal, 1)
-    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-    println(<-ch)
-
-    stop(c, wg)
+	stop(c, wg)
 }
-
-
